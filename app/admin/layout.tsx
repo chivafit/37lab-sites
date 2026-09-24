e86573b667aaeddef3fa812f60e22f6cf06abe2b
analyzA.I <contato@analyzai.app>
2026-09-24 08:47:42 -0300

    feat: enable clickable pending popover in admin

--- app/admin/layout.tsx
+++ app/admin/layout.tsx
@@ -1,7 +1,8 @@
 import {ReactNode} from 'react';
 import AdminGate from './AdminGate';
+import PendingPopover from './PendingPopover';
 import './admin-editor-fix.css';
 
 export default function AdminLayout({children}:{children:ReactNode}){
-  return <AdminGate>{children}</AdminGate>;
+  return <AdminGate>{children}<PendingPopover/></AdminGate>;
 }
